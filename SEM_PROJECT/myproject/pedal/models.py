36ccf10f1d7cdd447f965ad7458e88bd0912e2a8
Abhay <abhaytripathi98.com>
2023-11-15 00:30:18 +0530

    first commit

---
+++ SEM_PROJECT/myproject/pedal/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from .managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
+
+# Create your models here.
+# class AppUser(AbstractUser, PermissionsMixin):
+#     #username=None
+#     bits_id=models.CharField(unique=True,max_length=50)
+#     profile_img    = models.ImageField(upload_to='images/Users/', null=True, blank=True, default=None)
+#     # first_name = models.CharField(_("first_name"),max_length=50)
+#     # last_name =  models.CharField(_("last_name"),max_length=50)
+#     email = models.EmailField(_("email address"), unique=True)
+#     phone = models.CharField(max_length=15)
+#     address =  models.CharField(max_length=100)
+    
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = []
+#     objects = CustomUserManager()
+    
+
+class AppUser(models.Model):
+   #bits_id=models.CharField(unique=True,max_length=50)
+   profile_img    = models.ImageField(upload_to='images/Users/', null=True, blank=True, default=None)
+   #first_name = models.CharField(_("first_name"),max_length=50)
+   #last_name =  models.CharField(_("last_name"),max_length=50)
+   #email = models.EmailField(_("email address"), unique=True)
+   address =  models.CharField(max_length=100)
+   phone = models.CharField(max_length=15)
+   authUser=models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+class Cycle(models.Model):
+   cycle_img    = models.ImageField(null=True, blank=True, upload_to="images/")
+   owner =models.ForeignKey(AppUser, blank=True, null=True, on_delete=models.SET_NULL)
+   dop = models.DateTimeField('Date of Purchase')
+   model = models.CharField(max_length=50)
+   price=models.IntegerField()
+   lend_or_sell = models.CharField(max_length=50)
+   description = models.CharField(max_length=5000)
+   is_avail=models.BooleanField(default=True)
+   is_being_rented=models.BooleanField(default=False)
+   description = models.CharField(max_length=5000,null=True, blank=True)
+   no_of_rents = models.IntegerField(default=0)
+   total_stars=models.IntegerField(default=0)
+
+   #Cycle(model='hero Razor back',address='1102 MSA 1 ',dop='06/09/19',price='190',img='cycle2.jpg')
+
+class Order(models.Model):
+   razorpay_order_id=models.CharField(unique=True,max_length=50)
+   payment_staus=models.CharField(max_length=50)
+   user =models.ForeignKey(AppUser, blank=True, null=True, on_delete=models.SET_NULL)
+   cycle =models.ForeignKey(Cycle, blank=True, null=True, on_delete=models.SET_NULL)
+
+class Payment(models.Model):
+   razorpay_payment_id=models.CharField(unique=True,max_length=50)
+   razorpay_order_id=models.CharField(unique=True,max_length=50)
+   razorpay_signature=models.CharField(max_length=1000)
+   # payment_staus=models.CharField(max_length=50)
+   # user =models.ForeignKey(AppUser, blank=True, null=True, on_delete=models.SET_NULL)
+   # cycle =models.ForeignKey(Cycle, blank=True, null=True, on_delete=models.SET_NULL)
+   
+class Rent(models.Model):
+   payment=models.ForeignKey(Payment, blank=True, null=True, on_delete=models.SET_NULL)
+   #order=models.ForeignKey(Order, blank=True, null=True, on_delete=models.SET_NULL)
+   user =models.ForeignKey(AppUser, blank=True, null=True, on_delete=models.SET_NULL)
+   cycle =models.ForeignKey(Cycle, blank=True, null=True, on_delete=models.SET_NULL)
+   start_time=models.DateTimeField()
+   end_time=models.DateTimeField()
+   is_avail=models.BooleanField(default=True)
+   
---
+++ SEM_PROJECT/myproject/pedal/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
+    path("login", views.login_user, name="login_user"),
+    path("register", views.register_user, name="register_user"),
+    path("logout", views.logout_user, name="logout_user"),
+    path("sell", views.sell, name="sell"),
+    path("history", views.history, name="history"),
+    path("reports", views.reports, name="reports"),
+    path("shops", views.shops, name="shops"),
+    path("buy", views.buy, name="buy"),
+    path("checkout", views.checkout, name="checkout"),
+    path("details/<int:id>", views.details, name="details"),
+    path("payments", views.payments, name="payments"),
+    path("rentedBikes", views.rented_bikes, name="rented_bikes"),
+    path("ownedBikes", views.owned_bikes, name="owned_bikes"),
+    path("lend", views.lend, name="lend")
+    
+    
+
+]
